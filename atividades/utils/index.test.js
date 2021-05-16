4fb5377ad76830c30f803f870bf53bf8de28bbf3
Luiza <luizaleitemendes@gmail.com>
2021-05-15 22:56:22 -0300

    Testing github actions

--- atividades/utils/index.test.js
+++ atividades/utils/index.test.js
@@ -2,7 +2,7 @@ const { upperCaseString } = require("./index");
 
 describe("Testing upperCaseString(str)", () => {
   it("should return first letter uppercase - case one uppercase word", () => {
-    expect(upperCaseString("HELLO")).toEqual("Hello");
+    expect(upperCaseString("HELLO")).toEqual("HellO");
   });
 
   it("should return first letter uppercase - case one lowercase word", () => {
